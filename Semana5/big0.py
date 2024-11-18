bab9d63694439688a94823310bd56a856dd514bb
Alisson Barillas <Issnbarillas@gmail.com>
2024-11-17 18:02:32 -0600

    big 0

---
+++ Semana5/big0.py
@@ -0,0 +1,43 @@
+"""Algoritmo
+Binary Search
+Mejor caso: O(1) 
+Caso Promedio: O(Logn)
+Peor caso: O(Logn)
+
+
+Algoritmo 
+bubble sort
+Mejor caso: 0(n)
+Caso Promedio: O(n^2)
+Peor caso: O(n^2)
+
+Algoritmo
+Selection Sort
+Mejor caso: O(n^2)
+Caso Promedio: O(n^2)
+Peor caso: O(n^2)
+
+Algoritmo
+Insertion Sort
+Mejor caso: O(n)
+Caso Promedio: O(n^2)
+Peor caso: O(n^2)
+
+Algoritmo
+Merge Sort
+Mejor caso: O(nlogn)
+Caso Promedio: O(nlogn)
+Peor caso: O(nlogn)
+
+Algoritmo
+Quick Sort
+Mejor caso: O(nlogn)
+Caso Promedio: O(nlogn)
+Peor caso: O(n^2) """
+
+
+
+
+
+
+
